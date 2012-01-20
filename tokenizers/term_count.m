b33722030b572244f2835aabff04056cfbc7de5c
Mark <faridani@gmail.com>
2012-01-19 23:05:42 -0800

    .

---
+++ tokenizers/term_count.m
@@ -0,0 +1,11 @@
+function output = term_count(inputtext, headers)
+% takes:
+%      inputtext: a long string
+%      headers: a cell array containing a number of keywords
+% output:
+%      an array of numbers
+%      showing how many times each term is repeated in the text
+
+
+
+end
