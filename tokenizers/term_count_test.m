a8f2d9ce3a94031bd5c45d582554a14be8f8bc2e
Mark <faridani@gmail.com>
2012-01-19 23:18:53 -0800

    .

---
+++ tokenizers/term_count_test.m
@@ -0,0 +1,5 @@
+term_count('I love books', {'love','book'})
+term_count('hello', {'love','book'})
+
+term = 'a way of swimming by lying on your front and moving your arms together over your head while your legs move up and down'
+term_count(term, {'a','way', 'front', 'your'})
