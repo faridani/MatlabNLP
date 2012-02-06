f393cad4eec289bfaf80448925c5014aceef3ef9
Mark <faridani@gmail.com>
2012-02-06 13:08:12 -0800

    Sentiment Analysis example is pretty complete now

--- examples/sentiment/main.m
+++ examples/sentiment/main.m
@@ -1,11 +1,12 @@
 clc;
 clear all;
 close all;
+tic;
 
 addpath('funcs');
 negfiles = getAllFiles('data\txt_sentoken\neg\');
 posfiles = getAllFiles('data\txt_sentoken\pos\');
-labels = [zeros(size(negfiles,1),1); zeros(size(posfiles,1),1)];
+labels = [zeros(size(negfiles,1),1); ones(size(posfiles,1),1)];
 
 allfiles = [negfiles;posfiles];
 mycellarray ={};
@@ -26,6 +27,31 @@ mycellarray = mycellarray';
 %mycellarray2 = mycellarray;
 %mycellarray = mycellarray(1:5);
 
-featureVector = featurize(mycellarray, 1, 0, 0);
 
+featureVector = featurize(mycellarray, 20, 0, 0);
+
+save('featureVectorn50.dump','featureVector')
+
+%10 fold validation 
+Fresults = []
+for i = 1:10
+    randomindices = randperm(2000);
+    randomindices = randomindices(1:1800);
+    otherindices = (1:2000)';
+    testsetindex = setdiff(otherindices,randomindices)';
+    trainingsetindex = randomindices ;
+trainingset = featureVector(trainingsetindex,:);
+traininglabel = labels(trainingsetindex,:);
+
+testset = featureVector(testsetindex,:);
+testlabel = labels(testsetindex,:);
+O1 = NaiveBayes.fit(trainingset,traininglabel,'dist','mn'); % or  'mvmn'
+C2 = O1.predict(testset);
+cMat2 = confusionmat(testlabel,C2);
+Fresults = [Fresults,F1measureConfusionMatrix(cMat2)];
+end
+
+mean(Fresults)
+
+toc;
 
